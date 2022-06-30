0c9f554720ad4332ce0a7c73bf56dbda1ad43dc6
Akorede <emiolasaheed1@gmail.com>
2022-06-30 16:18:26 -0700

    art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main prints with proper grammar
+ * main- prints with proper grammar
  * but the outcome is a piece of art
- * Return 0 if exited properly
+ * Return: 0 if exited properly
  */
 int main(void)
 {
