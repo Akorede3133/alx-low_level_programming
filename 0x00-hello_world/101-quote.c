226eb728c417f8fb399d78c77994887b8a86e7c6
Akorede <emiolasaheed1@gmail.com>
2022-06-30 17:26:21 -0700

    size

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Prints and that art is useful
+ * Return: 0  if successful
+ */
+int main(void)
+{
+	char str[]="and that piece of art is useful\" - Dora Korpa, 2015-10-19\n";
+	fwrite(2,str,59);
+	return (1);
+}
