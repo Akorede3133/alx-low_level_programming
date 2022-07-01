afa011a3678a1afe48ab28881ac30e733d1c2f45
Akorede <emiolasaheed1@gmail.com>
2022-06-30 17:55:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
 /**
  * main- Prints and that art is useful
  * Return: 0  if successful
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		fprintf(stderr, "%s\n", str);
-			return (1);
-}
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = strlen(str);
+
+
+	write(2, str, length);
+	return (1);
+}	
