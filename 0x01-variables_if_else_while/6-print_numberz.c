b7308f1efed2cb5b985b74f4324d771c0ec4fee2
Akorede <emiolasaheed1@gmail.com>
2022-07-01 19:05:43 -0700

    nawa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Print numbers
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n%10 + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
