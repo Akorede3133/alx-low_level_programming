06924d8bb63dead97fd8ca26aedfaac0bf870c14
Akorede <emiolasaheed1@gmail.com>
2022-07-05 15:19:36 -0700

    print alpha

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -4,3 +4,17 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void print_alphabet(void)
+{
+	int l;
+
+	l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l++;
+	}
+	_putchar('\n');
+}
