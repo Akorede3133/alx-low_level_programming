e675f82a9c7b24ee20d02a722cfe365b96a6086c
Akorede <emiolasaheed1@gmail.com>
2022-07-06 15:36:49 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,28 +12,37 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i > 99)
 			{
 				_putchar(i / 100 + '0');
 				_putchar(i / 10 % 10 + '0');
 				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else if  (i > 9)
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
@@ -45,14 +54,20 @@ void print_to_98(int n)
 			{
 				_putchar(i / 10 + '0');
 				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 			else
 			{
 				_putchar(i % 10 + '0');
-				_putchar(',');
-				_putchar(' ');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
