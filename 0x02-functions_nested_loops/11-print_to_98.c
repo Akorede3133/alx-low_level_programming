53a47bbac7dc5b95b67a980657cf0ed5170afe91
Akorede <emiolasaheed1@gmail.com>
2022-07-06 16:10:00 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,60 +9,31 @@
 
 void print_to_98(int n)
 {
+	greater(n);
+	less(n);
+}
+void greater(int n)
+{
 	int i;
 
-	if (n > 98)
+	for (i = n; i >= 98; i++)
 	{
-		for (i = n; i >= 98; i--)
+		printf("%d", i);
+		if (i != 98)
 		{
-			if (i > 99)
-			{
-				printf("%d", i);
-				if (i != 98)
-				{
-					printf("%c%c", ',', ' ');
-				}
-			}
-			else if  (i > 9)
-			{
-				printf("%d", i);
-				if (i != 98)
-				{
-					printf("%c%c", ',' ' ');
-				}
-			}
-			else
-			{
-				_putchar(i % 10 + '0');
-				if (i != 98)
-				{
-					printf("%c%c", ',', ' ');
-				}
-			}
+			printf("%c%c", ',', ' ');
 		}
-		_putchar('\n');
 	}
-	else
+}
+
+void less(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i > 9)
-			{
-				printf("%d", i);
-				if (i != 98)
-				{
-					printf("%c%c", ',', ' ');
-				}
-			}
-			else
-			{
-				_putchar(i % 10 + '0');
-				if (i != 98)
-				{
-					printf("%c%c", ',', ' ');
-				}
-			}
-		}
-		_putchar('\n');
+		printf("%d", i);
+		if (i != 98)
+			printf("%c%c", ',', ' ');
 	}
 }
