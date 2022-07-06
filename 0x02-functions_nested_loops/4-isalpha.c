d450d4c0384e0879b6ebcf1d22f7fe1d69f0c4cb
Akorede <emiolasaheed1@gmail.com>
2022-07-05 18:28:11 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int_isalpha - checks for alphabets
+ * _isalpha - checks for alphabets
  * @c: paramter to check
  * Return: 0 on succes
  */
@@ -11,7 +11,7 @@ int _isalpha(int c)
 	int result;
 
 
-	if (c <= 122 && c>= 65)
+	if (c <= 122 && c >= 65)
 		result = 1;
 	else
 		result = 0;
