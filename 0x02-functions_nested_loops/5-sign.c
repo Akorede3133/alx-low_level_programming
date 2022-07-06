002cc23e31dfe48891bf61fcc14bbc35db9cdb7b
Akorede <emiolasaheed1@gmail.com>
2022-07-05 18:39:17 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main"
+
+/**
+ * print_sign - prints sign
+ * @n: parameter
+ * Return: 0 always
+ */
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		result = 0;
+		_putchar('0');
+	}
+	else
+	{
+		result = -1;
+		_putchar('-');
+	}
+	return (result);
+}
