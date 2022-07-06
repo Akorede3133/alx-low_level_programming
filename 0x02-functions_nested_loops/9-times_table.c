ff69d153b20735f9e6c01d6ee8181d631cf48fb6
Akorede <emiolasaheed1@gmail.com>
2022-07-06 07:35:19 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,25 +14,25 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			int res = i * j;
-
+		
 			if (res < 10)
 			{
-				_putchar(res % 10 + '0');
-				if (j != 9)
+				if (j == 0)
+					_putchar(res % 10 + '0');
+				else
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(res % 10 + '0');
 				}
 			}
 			else
-			{	
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(res / 10 + '0');
 				_putchar(res % 10 + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
