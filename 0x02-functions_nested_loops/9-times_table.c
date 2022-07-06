204c0f1e3d60ce10692e841269fa13e8556fa5cb
Akorede <emiolasaheed1@gmail.com>
2022-07-06 06:35:44 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - print timestable
+ * Return: 0 always
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int res = i * j;
+
+			if (res < 10)
+			{
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(res % 10 b + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
