f93493bcc35c42de700f7394cf52df6a287502f8
Akorede <emiolasaheed1@gmail.com>
2022-07-06 15:31:09 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,50 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		if (i < 10)
+		for (i = n; i > 98; i--)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i + '0');
-			if ( i != 98)
+			if (i > 99)
 			{
+				_putchar(i / 100 + '0');
+				_putchar(i / 10 % 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if  (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
 		}
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
 }
