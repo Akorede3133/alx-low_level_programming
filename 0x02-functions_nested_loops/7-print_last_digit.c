3f26617372fa3f14181406cb29c28686c98573ff
Akorede <emiolasaheed1@gmail.com>
2022-07-06 02:02:46 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: parameter
+ * Return: 0 always
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	return (last);
+}
