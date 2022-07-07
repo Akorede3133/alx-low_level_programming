d30227713dfdedb7487f55817b359d1ffa05d6e4
Akorede <emiolasaheed1@gmail.com>
2022-07-07 07:55:37 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two ints
+ * @a: first para
+ * @b: sec para
+ * Return: 0 success
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
