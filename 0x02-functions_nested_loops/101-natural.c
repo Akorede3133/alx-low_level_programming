2dd37df2712d0b61f3523d01ca812b6e62b52037
Akorede <emiolasaheed1@gmail.com>
2022-07-07 04:50:48 -0700

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints sum
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
