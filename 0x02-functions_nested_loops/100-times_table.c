38190bae055ff28c429b21febff4ef129093c01d
Akorede <emiolasaheed1@gmail.com>
2022-07-06 17:34:59 -0700

    print

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,35 +13,36 @@ void print_times_table(int n)
 	if (n < 16 && n > 0)
 	{
 
-	for (i = 0; i < n + 1; i++)
-	{
-		for (j = 0; j < n + 1; j++)
+		for (i = 0; i < n + 1; i++)
 		{
-			int res;
-
-			res = i * j;
-			if (res < 10)
+			for (j = 0; j < n + 1; j++)
 			{
-				if (j == 0)
-					_putchar(res % 10 + '0');
+				int res;
+
+				res = i * j;
+				if (res < 10)
+				{
+					if (j == 0)
+						_putchar(res % 10 + '0');
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(res % 10 + '0');
+					}
+				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
+					_putchar(res / 10 + '0');
 					_putchar(res % 10 + '0');
 				}
 			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
