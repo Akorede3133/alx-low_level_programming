5a5cde715c61d449fd8777520132f5133df2c9b3
Akorede <emiolasaheed1@gmail.com>
2022-07-06 17:17:55 -0700

    print

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - print timestable
+ * @n: para
+ * Return: 0 always
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n + 1; i++)
+	{
+		for (j = 0; j < n + 1; j++)
+		{
+			int res;
+
+			res = i * j;
+			if (res < 10)
+			{
+				if (j == 0)
+					_putchar(res % 10 + '0');
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res % 10 + '0');
+				}
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
