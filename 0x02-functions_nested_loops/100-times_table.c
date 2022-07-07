0048ab9ae8ede0f69f384cd9ec0a33e1d42e2246
Akorede <emiolasaheed1@gmail.com>
2022-07-06 17:40:33 -0700

    print

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,12 +33,20 @@ void print_times_table(int n)
 						_putchar(res % 10 + '0');
 					}
 				}
-				else
+				else if (res < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(res / 100 + '0');
+					_putchar(res / 10 % 10 + '0');
 					_putchar(res % 10 + '0');
 				}
 			}
