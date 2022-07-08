c6782efb904ae9bc2be5bae1172a46faf048cebd
Akorede <emiolasaheed1@gmail.com>
2022-07-08 15:42:30 -0700

    tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * print_triangle - print triangle
+ * @size: para
+ * Return: 0 success
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = n; j >= i; j--)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		if (i < n)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
