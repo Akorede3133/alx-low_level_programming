db41858689489f1e5090d0aadcc2db1800b0d88d
Akorede <emiolasaheed1@gmail.com>
2022-07-21 23:49:48 -0700

    by 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,12 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *str; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str = str + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
