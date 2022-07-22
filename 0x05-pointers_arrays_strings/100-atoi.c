4276149871c0cfab1e402d0ec97000195b2ab199
Akorede <emiolasaheed1@gmail.com>
2022-07-22 00:43:03 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _atoi - converts to int
+ * @s: para
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i, num, neg;
+
+	num = 0;
+	neg = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+			num = num * 10 + (s[i] - 48);
+		if (s[i] == '-')
+			neg = -1;
+	}
+	return (num * neg);
+}
