a6d4b24904b81083e2c634a089749ed18fd9fd74
Akorede <emiolasaheed1@gmail.com>
2022-07-21 23:28:07 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse
+ * @s: para
+ */
+int length(char *s)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
+void rev_string(char *s)
+{
+	int i, len;
+	char *end, temp, *beg;
+
+	len = length(s);
+	end = s;
+	beg = s;
+
+	for (i = 0; i < len - 1; i++)
+		end++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = *end;
+		*end = *beg;
+		*beg = temp;
+		end--;
+		beg++;
+	}
+}
