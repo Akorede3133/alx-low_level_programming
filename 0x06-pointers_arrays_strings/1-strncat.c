74502ffb1d73a02af087968c6746c4c07f6cfa56
Akorede <emiolasaheed1@gmail.com>
2022-07-22 02:09:12 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - strcat
+ * @dest: dest
+ * @src: src
+ * @n: num
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		i++;
+	}
+	
+	j = 0;
+
+	for (j = 0; j < n && src[i] != '\0'; j++)
+	{
+		dest[i + j] = src[i];
+	}
+	for (; j < n; j++)
+		dest[i + j] = '\0';
+	return (dest);
+}
