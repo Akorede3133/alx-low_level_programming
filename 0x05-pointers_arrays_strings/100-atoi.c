0a2d0618da2b025469f86ec8b948e3a76ad2ed6f
Akorede <emiolasaheed1@gmail.com>
2022-07-22 00:47:36 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,9 +16,11 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 48 && s[i] <= 57)
+		{
 			num = num * 10 + (s[i] - 48);
-		if (s[i] == '-')
-			neg = -1;
+			if (s[i - 1] == '-')
+				neg = -1;
+		}
 	}
 	return (num * neg);
 }
