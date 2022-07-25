f3331c3eed30dd8c0ab4352fd8bd1280ddb0f311
Akorede <emiolasaheed1@gmail.com>
2022-07-25 16:35:12 -0700

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - rev array
+ * @a: array
+ * @n: num
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[n - i - 1];
+		a[n - i -1] = a[i];
+		a[i] = temp;
+	}
+}
