2de698662cfe55657672db3e23418b464649a8cd
Akorede <emiolasaheed1@gmail.com>
2022-07-25 16:04:34 -0700

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,21 +9,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, res;
+	int i;
 
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i] && s1[i] > s2[i])
 		{
-			res = s1[i] - s2[i];
+			return (s2[i] - s1[i]);
 		}
 		else if (s1[i] != s2[i] && s2[i] > s1[i])
 		{
-			res = s2[i] - s1[i];
+			return (s2[i] - s1[i]);
 		}
-		else 
-			res = 0;
+		else if (s1[i] == s2[i])
+		{
+			return (0);
+		}
 		return (res);
 	}
 }
