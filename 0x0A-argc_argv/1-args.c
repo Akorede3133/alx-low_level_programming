10bbdbc1991873c49cf706cf256ad6c3548174a9
Akorede <emiolasaheed1@gmail.com>
2022-07-26 16:35:32 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 Always
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
