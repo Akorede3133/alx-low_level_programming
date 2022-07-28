aa144df36821ebc536a10a26798f807f33385c69
Akorede <emiolasaheed1@gmail.com>
2022-07-28 16:44:16 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy
+ * @str: para
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i, len;
+
+	len = 0;
+	while (str[i] != '\0')
+	{
+		len++;
+	}
+	ptr = malloc(sizeof(char) * len);
+
+	if (ptr != NULL)
+	{
+		if (str != NULL)
+		{
+			for (i = 0; str[i] != '\0'; i++)
+			{
+				ptr[i] = str[i];
+			}
+		}
+		else
+		{
+			ptr = NULL;
+		}
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
