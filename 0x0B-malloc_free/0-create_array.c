790439091e6927f635eac2fe46a4bd8770f65403
Akorede <emiolasaheed1@gmail.com>
2022-07-28 16:25:54 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,9 +17,12 @@ char *create_array(unsigned int size, char c)
 
 	if (ptr != NULL)
 	{
-		for (i = 0; i < size; i++)
+		if (size != 0)
 		{
-			ptr[i] = c;
+			for (i = 0; i < size; i++)
+			{
+				ptr[i] = c;
+			}
 		}
 		return (ptr);
 	}
