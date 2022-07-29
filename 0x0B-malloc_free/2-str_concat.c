a9237a84af40bc3639989758ac40fa6c7a3b0de0
Akorede <emiolasaheed1@gmail.com>
2022-07-28 17:33:36 -0700

    concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concat
+ * @s1: para
+ * @s2: para
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	int i, len1, len2, j;
+
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
+	ptr = malloc(sizeof(char) * (len1 + len2) + 2);
+
+	if (ptr != NULL)
+	{
+		if (s1 == NULL && s2 == NULL)
+		{
+			ptr = NULL;
+		}
+		else
+		{
+			for (i = 0; s1[i] != '\0'; i++)
+			{
+				ptr[i] = s1[i];
+			}
+			for (j = 0; s2[i] != '\0'; j++)
+			{
+				ptr[i + j] = s2[j];
+			}
+			ptr[i + j] = '\0';
+		}
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
