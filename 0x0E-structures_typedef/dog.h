c260f6ff988130e2d1a50e386e4efd64183a734d
Akorede <emiolasaheed1@gmail.com>
2022-08-12 05:13:41 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
