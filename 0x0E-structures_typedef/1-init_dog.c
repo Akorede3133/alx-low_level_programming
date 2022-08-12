2e399663fe3055a171600f4764cb2b796deeb2ef
Akorede <emiolasaheed1@gmail.com>
2022-08-12 05:18:10 -0700

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - init dog
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
