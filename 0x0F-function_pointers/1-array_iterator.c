768b73a34f3f2f2fa9907f414646ec2740200538
Akorede <emiolasaheed1@gmail.com>
2022-08-16 01:51:25 -0700

    iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterator
+ * @array: arr
+ * @size: size
+ * @action: action
+ *
+ * Return: Nothinh
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
