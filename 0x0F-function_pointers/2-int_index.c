10ab10096e1c88b600db8dee54269169b9cd7f5f
Akorede <emiolasaheed1@gmail.com>
2022-08-16 04:12:25 -0700

    cmp

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,13 +17,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	else
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			break;
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
+		return (-1);
 	}
-	return (-1);
 }
