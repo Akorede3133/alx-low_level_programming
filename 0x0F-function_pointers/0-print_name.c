98fc3678eb90712118620b28309745925a3f965b
Akorede <emiolasaheed1@gmail.com>
2022-08-16 01:40:57 -0700

    do

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "function_pointers.h"
 /**
  * print_name - print name
  * @name: name
  * @f: func
- * Return: nothing
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
