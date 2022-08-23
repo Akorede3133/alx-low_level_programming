1464b0e1cf9abee413c7f7406aa90c1ac14859af
Akorede <emiolasaheed1@gmail.com>
2022-08-23 00:04:03 -0700

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+/**
+ * read_textfile - read text
+ * @filename: file name
+ * @letters: letters
+ * Return: int
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t sz;
+	int fd;
+
+	if (!filename)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDRW);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	sz = read(fd, (void *)filename, letters);
+	return (sz);
+}
